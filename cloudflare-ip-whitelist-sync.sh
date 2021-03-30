e87e908fdc5825e7b027b22fbcb06cf8a1db1c5f
Shimul <shimuldn@users.noreply.github.com>
2021-03-30 13:58:50 +0530

    Create cloudflare-ip-whitelist-sync.sh

---
+++ cloudflare-ip-whitelist-sync.sh
@@ -0,0 +1,25 @@
+mkdir /opt/scripts
+nano /opt/scripts/cloudflare-ip-whitelist-sync.sh
+#!/bin/bash
+
+CLOUDFLARE_FILE_PATH=/etc/nginx/cloudflare
+
+echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
+echo "" >> $CLOUDFLARE_FILE_PATH;
+
+echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
+for i in `curl https://www.cloudflare.com/ips-v4`; do
+    echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+done
+
+echo "" >> $CLOUDFLARE_FILE_PATH;
+echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
+for i in `curl https://www.cloudflare.com/ips-v6`; do
+    echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+done
+
+echo "" >> $CLOUDFLARE_FILE_PATH;
+echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
+
+#test configuration and reload nginx
+nginx -t && systemctl reload nginx
